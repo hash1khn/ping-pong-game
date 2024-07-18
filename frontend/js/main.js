0cb95f1832ed0843c5b1c6071fcfff37e62a2db6
Hashir Khan <hashirahmedkhan123@gmail.com>
2024-07-18 16:22:07 +0500

    updated

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -10,7 +10,6 @@ let startBtn = document.getElementById('startBtn');
 startBtn.addEventListener('click', startGame);
 
 
-
 let message = document.getElementById('message');
 
 
@@ -18,9 +17,9 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
 
-let player1;
-let player2;
-let ball;
+let player1
+let player2
+let ball
 
 let isGameStarted = false;
 let playerNo = 0;
@@ -29,7 +28,6 @@ let roomID;
 const socket=io("http://localhost:3000",{
     transports:['websocket']
 })
-
 function startGame() {
     startBtn.style.display = 'none';
 
@@ -40,42 +38,79 @@ function startGame() {
     else{
         message.innerText='please wait before starting'
     }
+} 
+socket.on('playerNo',(newplayerNo)=>{
+    console.log("Player No" + newplayerNo)
+    playerNo= newplayerNo
+});
+socket.on('startGame',()=>{
+    isGameStarted=true
+    message.innerText="We are going to start a new game...."
 
+});
 
+socket.on('GameStarted',(room)=>{
+    roomID=room.id
+    message.innerText=""
+    player1=new Player(room.players[0].x,room.players[0].y,20,60,'red');
+    player2=new Player(room.players[1].x,room.players[1].y,20,60,'blue');
+    player1.score=room.players[0].score
+    player2.score=room.players[1].score
+    ball=new Ball(room.ball.x,room.ball.y,10,'white');
     window.addEventListener('keydown', (e) => {
         if (isGameStarted) {
-            if (e.keyCode === 38) {
-                console.log("player move 1 up")
-                socket.emit("move", {
-                    roomID: roomID,
-                    playerNo: playerNo,
-                    direction: 'up'
-                })
-            } else if (e.keyCode === 40) {
-                console.log("player move 1 down")
-                socket.emit("move", {
-                    roomID: roomID,
-                    playerNo: playerNo,
-                    direction: 'down'
-                })
-            }
+            console.log("player is moving 1 up ")
+                if(e.keyCode===38){
+                    socket.emit('move',{
+                        roomID:roomID,
+                        playerNo:playerNo,
+                        direction:'up'
+                    })
+                }else if(e.keyCode ===40){
+                    socket.emit('move',{
+                        roomID:roomID,
+                        playerNo:playerNo,
+                        direction:'down'
+                    })
+                    
+                }
+            // else if (playerNo===2) {
+            //     if(e.keyCode ===38){
+            //         socket.emit('move',{
+            //             roomID:roomID,
+            //             playerNo:playerNo,
+            //             direction:'up'
+            //         })
+            //     } else if(e.keyCode ===40){    
+            //         socket.emit('move',{
+            //             roomID:roomID,
+            //             playerNo:playerNo,
+            //             direction:'down'
+            //         }) 
+            //     }
+            // }
         }
     });
 
     draw();
-}
+})
+
 
 socket.on("updateGame", (room) => {
-    player1.y = room.players[0].y;
-    player2.y = room.players[1].y;
+    player1.x=room.players[0].x
+    player1.y=room.players[0].y
+    player2.x=room.players[1].x
+    player2.y=room.players[1].y
 
-    player1.score = room.players[0].score;
-    player2.score = room.players[1].score;
+    player1.score=room.players[0].score
+    player2.score=room.players[1].score
 
-    ball.x = room.ball.x;
-    ball.y = room.ball.y;
+    ball.x=room.ball.x
+    ball.y=room.ball.y
 
-    draw();
+    draw()
+
+
 });
 
 socket.on("endGame", (room) => {
